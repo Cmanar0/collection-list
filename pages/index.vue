c9216712fb3c9d51ae5473f66d58860d02083379
Marian Adamus <cmanar0@gmail.com>
2024-02-05 16:52:57 +0100

    add axios

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,26 @@
 </template>
 
 <script>
-export default {};
+// Import the Axios instance
+import apiClient from "~/services/apiService";
+
+export default {
+  data() {
+    return {
+      collections: [],
+    };
+  },
+  async mounted() {
+    try {
+      // Call the API endpoint using Axios in the mounted hook
+      const response = await apiClient.get("/collections");
+      this.collections = response.data;
+    } catch (error) {
+      console.error("Error fetching collections:", error);
+      this.collections = [];
+    }
+  },
+};
 </script>
 
 <style></style>
