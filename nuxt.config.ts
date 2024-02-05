b7c2503f4f7ce469df471e1934b9cb8d4acbf590
Marian Adamus <cmanar0@gmail.com>
2024-02-05 16:41:05 +0100

    create home page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  css: ["~/assets/styles.css"],
+  // Your existing configurations
+  devtools: { enabled: true },
+
+  // Vuetify-specific configurations
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container class="my-container">
+    <v-row justify="center">
+      <v-col cols="12" md="6">
+        <!-- Left Card -->
+        <v-card class="pa-5 my-card" outlined>
+          <!-- Content for the left card -->
+          Home Page
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
